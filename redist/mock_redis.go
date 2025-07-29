78daf419600cafab8f6ccf063d690d5041de4aec
gopher <gophero@proton.me>
2025-07-29 10:42:49 +0000

    fix: correct return type

--- redist/mock_redis.go
+++ redist/mock_redis.go
@@ -8,21 +8,21 @@ import (
 
 	miniredis "github.com/alicebob/miniredis/v2"
 	redismock "github.com/go-redis/redismock/v9"
-	rds "github.com/redis/go-redis/v9"
+	"github.com/redis/go-redis/v9"
 )
 
-func MockRedis() (*rds.Client, redismock.ClientMock) {
+func MockRedis() (*redis.Client, redismock.ClientMock) {
 	return redismock.NewClientMock()
 }
 
-func NewMiniRedis() *rds.Client {
+func NewMiniRedis() *redis.Client {
 	// 测试用miniredis
 	mr, err := miniredis.Run()
 	if err != nil {
 		panic(fmt.Errorf("new test redis error: %v", err))
 	}
 	// 使用miniredis创建client
-	client := rds.NewClient(&rds.Options{
+	client := redis.NewClient(&redis.Options{
 		Addr:         mr.Addr(),
 		Password:     "",
 		DB:           0,
@@ -41,7 +41,7 @@ func NewMiniRedis() *rds.Client {
 	return client
 }
 
-func NewRedisCluster() *rds.Client {
+func NewRedisCluster() redis.UniversalClient {
 	// TODO mock redis cluster
 	return nil
 }
